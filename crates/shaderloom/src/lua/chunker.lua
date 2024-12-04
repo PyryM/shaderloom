99ae36a755e3c07182747ebb1ed65523d5c6fb9a
PyryM <pkmatikainen@gmail.com>
2024-12-04 17:59:58 -0500

    stubs for chunker functions

--- crates/shaderloom/src/lua/chunker.lua
+++ crates/shaderloom/src/lua/chunker.lua
@@ -1,3 +1,34 @@
 -- does a thing
 
-print("Running chunker.lua!")
+local chunker = {}
+
+-- A chunk may be:
+-- {kind: "lua", source: string}
+-- {kind: "raw", source: string}
+-- {kind: "lua_expr", source: string}
+-- {kind: "mixed", chunk: chunk[]}
+
+---
+--- Split source code into a list of chunks
+---
+---@param src string
+---@return chunks table[]
+function chunker.split_source(src)
+    local chunks = {}
+
+
+
+    return chunks
+end
+
+---
+--- Formats chunks into an executable Lua script
+---
+---@param chunks chunk[]
+---@return string
+function chunker.emit_lua(chunks)
+    local frags = {}
+    return table.concat(frags, "\n")
+end
+
+return chunker
