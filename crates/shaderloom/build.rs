067b664b83dabaf6dd2781625cca5a9e86305a88
PyryM <pkmatikainen@gmail.com>
2024-12-04 16:40:37 -0500

    minor renaming

--- crates/shaderloom/build.rs
+++ crates/shaderloom/build.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 
 fn wrap_lua_file<P: AsRef<Path>>(name: &str, src: &P) -> String {
     let data = fs::read_to_string(src).expect("Failed to read file!");
-    format!("_p['{}'] = function()\n{}\nend", name, data)
+    format!("_EMBED['{}'] = function()\n{}\nend", name, data)
 }
 
 fn wrap_entry(entry: fs::DirEntry) -> String {
@@ -27,7 +27,7 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("embedded_lua_bundle.rs");
 
     let embeds = wrap_lua_source_files();
-    let source = format!("local _p={{}}\n{}\n_p['_init.lua']()", embeds);
+    let source = format!("local _EMBED={{}}\n{}\n_EMBED['_init.lua']()", embeds);
 
     fs::write(&dest_path, source).expect("Failed to write embedded bundle.");
     println!("cargo::rerun-if-changed=build.rs");
--- crates/shaderloom/src/lua/_init.lua
+++ crates/shaderloom/src/lua/_init.lua
@@ -2,18 +2,21 @@
 --
 -- Sets up Lua environment
 
-local _require = require
+-- `build.rs` embeds the lua source files in this directory (`src/lua/*.lua`)
+-- into a table of functions `_EMBED`:
+-- we replace `require` to first check if a file exists in that embedded table
 
-local _loaded = {}
+local _require = require
+local _LOADED = {}
 
 require = function(name)
     local fn = name:gsub("%.", "/") .. ".lua"
-    if _loaded[name] then return _loaded[name] end
-    if _p[fn] then 
+    if _LOADED[name] then return _LOADED[name] end
+    if _EMBED[fn] then 
         print(("Loading '%s'"):format(name))
-        _loaded[name] = _p[fn]()
-        if _loaded[name] == nil then _loaded[name] = true end
-        return _loaded[name]
+        _LOADED[name] = _EMBED[fn]()
+        if _LOADED[name] == nil then _LOADED[name] = true end
+        return _LOADED[name]
     end
     print(("Didn't find '%s', trying regular require!"):format(fn))
     return _require(name)
