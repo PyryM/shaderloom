bcbc77ad5c6dcb763a50680098a1dd0cb0a0c6ed
PyryM <pkmatikainen@gmail.com>
2025-07-16 20:21:23 -0400

    more flexibility in emitted rust structs

--- examples/wgpu_bundle/loom.lua
+++ examples/wgpu_bundle/loom.lua
@@ -1,8 +1,22 @@
 use "config.declarative"
 
 target "rust.wgpu" {
-    bundle = "${SCRIPTDIR}/shader_structs.rs",
     validate = true,
+    struct_definitions = {
+        output = "${SCRIPTDIR}/shader_structs.rs",
+        field_decorator = function(field, _ty)
+            -- add PyO3 getter/setter annotations to fields
+            field.comment = "/// A field visible to python!"
+            if not field.is_padding then
+                field.derive = "#[pyo3(get, set)]"
+            end
+        end,
+        struct_decorator = function(struct)
+            -- annotate this as a PyO3 class
+            struct.comment = "/// A fun struct visible to python!"
+            struct.derive = "#[derive(Copy, Clone, Pod, Zeroable)]\n#[pyclass]"
+        end
+    },
 }
 
 include_dirs "${SCRIPTDIR}/include"
